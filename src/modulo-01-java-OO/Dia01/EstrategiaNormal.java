d046eb1aee95323092bfb58dd3a14660104bb0ca
CassianoNeves <cassianodasnevesc10@gmail.com>
2015-04-16 10:51:02 -0300

    Add Estrategias deataque ao exercito de Elfos

---
+++ src/modulo-01-java-OO/Dia01/EstrategiaNormal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+/**
+ * Write a description of class EstrategiaNormal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EstrategiaNormal implements EstrategiaDeAtaque
+{
+    public void atacarOrcs(ArrayList<Elfo> elfos, ArrayList<Orc> orcs){
+        System.out.println("Estrategia normalzinha...");
+        
+        
+    }
+}
