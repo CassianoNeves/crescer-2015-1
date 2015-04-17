4815021c3c9bf6c6c5d9b51f4e1d94e642803b89
CassianoNeves <cassianodasnevesc10@gmail.com>
2015-04-17 11:34:21 -0300

    adicionando Tema 1

--- src/modulo-01-java-OO/Dia01/EstrategiaNormal.java
+++ src/modulo-01-java-OO/Dia01/EstrategiaNormal.java
@@ -1,15 +1,22 @@
 import java.util.*;
-/**
- * Write a description of class EstrategiaNormal here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class EstrategiaNormal implements EstrategiaDeAtaque
-{
-    public void atacarOrcs(ArrayList<Elfo> elfos, ArrayList<Orc> orcs){
-        System.out.println("Estrategia normalzinha...");
+
+public class EstrategiaNormal implements EstrategiaDeAtaque {
+    
+    private ArrayList<Elfo> ordemAtaque = new ArrayList<>();
+    
+    public void atacarOrcs(ArrayList<Elfo> elfos, ArrayList<Orc> orcs) {
+        System.out.println("Estratégia normalzinha...");
         
+        for (Elfo elfo : elfos) {
+            ordemAtaque.add(elfo);
+            for (Orc orc : orcs) {
+                elfo.atirarFlecha(orc);
+            }
+        }        
         
+    }
+    
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return ordemAtaque;
     }
 }
