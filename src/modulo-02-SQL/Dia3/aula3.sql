61fd9dca66ed77c266cb880e2c1fc15e3366e9ec
CassianoNeves <cassianodasnevesc10@gmail.com>
2015-04-20 17:58:19 -0300

    Mais Exercicios

--- src/modulo-02-SQL/Dia3/aula3.sql
+++ src/modulo-02-SQL/Dia3/aula3.sql
@@ -15,3 +15,24 @@ where nome = 'Uberlândia'
 select idcidade, nome
 from cidade
 where uf = 'RS'
+
+--Mais exercicios
+--1
+select SUBSTRING(Nome, 1, CHARINDEX(' ', nome))
+from associado
+
+--2
+select nome, 
+	DATEDIFF ( year , DataNascimento,  SYSDATETIME()) as Idade
+from Associado
+--3
+select nomeEmpregado, convert(datetime, DataAdmissao, 103) as Data_De_Emissao, 
+		DATEDIFF ( MONTH , convert(datetime, DataAdmissao, 103), convert(datetime, '31/12/2000', 103 ))
+from Empregado
+where DataAdmissao between convert(datetime, '01/05/1980', 103 ) and convert(datetime, '20/01/1982', 103 )
+--4
+select top 1 cargo
+from empregado
+--5
+select MAX(LEN(nome))
+from associado
