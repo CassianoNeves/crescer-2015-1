ab48fca00fe52a68aa740c66b3076db564fe4175
CassianoNeves <cassianodasnevesc10@gmail.com>
2015-04-22 21:59:07 -0300

    Mais exercicios de Banco

--- src/modulo-02-SQL/Dia3/aula3.sql
+++ src/modulo-02-SQL/Dia3/aula3.sql
@@ -45,7 +45,7 @@ where LEN(nome) = (Select MAX(LEN(nome)) from Associado)
 
 --6
 select nome, Dateadd( year, 50, DataNascimento ) as Aniversario_50_Anos, 
-			datename(dw, Dateadd( year, 50, DataNascimento ) )
+			datename(dw, Dateadd( year, 50, DataNascimento ) ) as Dia_da_Semana
 from associado
 
 --7
@@ -60,18 +60,48 @@ group by nome, UF
 having count(1) > 1
 
 --9
-select max(IDAssociado) + 1 as ID_do_proximo
+select ISNULL(max(IDAssociado),0) + 1 as ID_do_proximo
 from associado
 
 --10
-
 insert into cidadeaux (IDCidade, Nome, UF )
-(select  nome, UF
+(select min(IDCidade), nome, UF
 from cidade
-group by nome, UF
-having count(nome) = 1)
-
-select *
-from cidade
+group by nome, UF)
 
 truncate table cidadeaux
+
+--11)Altere todas cidades duplicadas (nome e uf iguais), acrescente no ínicio do nome um asterisco (*).
+
+begin transaction
+update cidade set nome = ('*'+ nome)
+where nome in (
+	select  nome
+	from cidade
+	group by nome
+	having count(1) > 1)
+
+commit
+
+rollback
+
+--12)Faça uma consulta que liste o nome do Associado e a descrição da coluna Sexo, informando: Masculino ou Feminino.
+
+select nome, replace(replace(sexo, 'M', 'Masculino'), 'F', 'Feminino')
+from associado
+
+--13)Faça uma consulta que mostre o nome do empregado, o Salario e percentual a ser descontado do Imposto de Renda, 
+--considerando a tabela abaixo:
+--Até R$ 1.164,00 = 0% De R$ 1.164,00 a R$ 2.326,00 = 15%
+--Acima de R$ 2.326,00 = 27,5%.
+
+select NomeEmpregado, salario, 
+	case
+		when salario < 1164 then '0%'
+		when salario > 1164 and salario < 2326 then '15%'
+		when salario > 2326 then '27,5%'
+	end
+from empregado
+
+--14)Elimine as cidades duplicadas (mantendo 1 registro para cada).
+--15)Adicione uma regra que impeça exista mais de uma cidade com o mesmo nome em um estado.
