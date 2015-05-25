52ef388da9c38b98513ef2e0974353d0c21a24ff
CassianoNeves <cassianodasnevesc10@gmail.com>
2015-05-24 23:51:51 -0300

    Versionando Filmator-Web

---
+++ src/modulo-04-java/filmator-web/src/main/java/filmator/model/Filme.java
@@ -0,0 +1,57 @@
+package filmator.model;
+
+public class Filme {
+
+	private String nome;
+	private Genero genero;
+	private int ano;
+	private String sinopse;
+	private String imagem;
+	
+	public Filme() {}
+	
+	public Filme( String nome ) {
+		this.nome = nome;
+	}
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getSinopse() {
+		return sinopse;
+	}
+
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+	
+	
+}
